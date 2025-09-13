8ba4576f5eb010a8b4ea7b420fa7dd7a83b3c182
ShashankVerma-1234bro <vermashashank070205@gmail.com>
2025-09-14 00:55:52 +0530

    added basic date picker.

--- app/(student)/(home)/index.tsx
+++ app/(student)/(home)/index.tsx
@@ -1,11 +1,23 @@
 import { View,  Text, } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "@/components/home/Card";
 import { useAppTheme } from "@/constants/themes/ThemeManager";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import NewButton from "@/components/NewButton";
 
 export default function HomeScreen() {
   const { styles } = useAppTheme();
   const screenStyles = styles.HomeScreen;
+
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const onChange = (_event: any, selectedDate?: Date) => {
+    setShow(false);
+    if (selectedDate) {
+      setDate(selectedDate);
+    }
+  };
 
   return (
     <View style={screenStyles?.container}> 
@@ -16,6 +28,19 @@ export default function HomeScreen() {
         heading={"The Support Shelf"} 
         description={"Explore self-help books and inspiring stories to guide your growth."} 
       />      
+
+      <Text>Selected: {date.toLocaleString()}</Text>
+
+      <NewButton title="Pick date/time" onPress={() => setShow(true)} />
+
+      {show && (
+        <DateTimePicker
+          value={date}
+          mode="date"        // or "time" / "datetime"
+          display="default"  // "spinner" / "calendar" / "clock"
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 }
